853c3a35b3e4231f4e1f5e08c9003bf3ae15c0fb
MrPetet <80315494+MrPetet@users.noreply.github.com>
2021-03-09 19:39:07 +0700

    Add files via upload

---
+++ plugins/info.js
@@ -0,0 +1,24 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  conn.reply(m.chat, `
+╠═〘 INFO BOT 〙 ═
+╠➥ Dibuat dengan bahasa javascript via NodeJs
+║
+╠➥ Instagram: @apml_gt
+║
+╠═〘 NfQ BOT 〙 ═
+`.trim(), m)
+}
+handler.command = /^(info)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
